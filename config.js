dc033e7d63bc63714123cb2f3635efc0740fc5de
gh <414058595@qq.com>
2017-03-16 15:30:18 +0800

    fix config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.dir = {
-    controller: '/static/controller',
-    util: '/static/common',
-    css: '/static/css'，
-		viewsDir : '/mnt/c/gh_work/intbee-parent/intbee-server/src/main/webapp/WEB-INF/views'
+	controller: '/static/controller',
+	util: '/static/common',
+	css: '/static/css',
+	viewsDir: '/mnt/c/gh_work/intbee-parent/intbee-server/src/main/webapp/WEB-INF/views'
 }
